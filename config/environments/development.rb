23728285a9a0d638901de45f8da450346c787676
Ramón Cahenzli <rca@psy-q.ch>
2015-09-10 17:56:15 +0200

    Use Rails I18n for model attributes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.gettext_i18n_rails.use_for_active_record_attributes = false
 end
