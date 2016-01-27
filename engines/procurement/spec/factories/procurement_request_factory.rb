1d7621601bf12d0b429283d4fabe3b20e933f78d
Matus Kmit <matus.kmit@zhdk.ch>
2016-01-27 17:45:48 +0100

    remove rand from factory

--- engines/procurement/spec/factories/procurement_request_factory.rb
+++ engines/procurement/spec/factories/procurement_request_factory.rb
@@ -1,9 +1,6 @@
 FactoryGirl.define do
   factory :procurement_request, class: Procurement::Request do
-    user { r = Procurement::Access.requesters.order('RAND()').first \
-                || FactoryGirl.create(:procurement_access, :requester)
-           r.user
-          }
+    association :user
     association :budget_period, factory: :procurement_budget_period
     association :group, factory: :procurement_group
     article_name { Faker::Lorem.sentence }
